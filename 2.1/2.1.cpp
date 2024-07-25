8e19a88fbe4f5daa83d43a2d8e081766e1ac5b7c
User <User@DESKTOP-KGVD8QQ>
2024-07-26 00:10:14 +0300

    Добавьте файлы проекта.

---
+++ 2.1/2.1.cpp
@@ -0,0 +1,19 @@
+﻿// 2.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    double z;
+    cout << "Прошло 234 дня.\n";
+    z = 234 / 7;
+    cout << "В 1-й неделе 7 дней.\n";
+    cout << "Число полных недель за этот период: " << floor(z) << endl;
+    _getch();
+}
